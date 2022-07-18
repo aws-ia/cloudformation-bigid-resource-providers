c0c0232d8feac172e38429584afc08f848763b3f
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2022-07-18 12:16:54 +0200

    Update handler to use the new BigID client

--- BigID-DataSource-S3/src/handlers.ts
+++ BigID-DataSource-S3/src/handlers.ts
@@ -1,209 +1,138 @@
-import {ConnectionResponse, ResourceModel, TypeConfigurationModel} from './models';
+import {Connection, ResourceModel, TypeConfigurationModel} from './models';
 import {AbstractBigIdResource} from "../../BigID-Common/src/abstract-bigid-resource"
-import axios from "axios";
+import {BigIdClient} from "../../BigID-Common/src/bigid-client"
 
-type SessionToken = {
-    "auth_token": string
+type ConnectionResponse = {
+    ds_connection: any
 }
 
 type ConnectionResponses = {
-    "ds_connections": ConnectionResponse[]
+    ds_connections: any[]
 }
 
-class Resource extends AbstractBigIdResource<ResourceModel, ConnectionResponse, ConnectionResponse, ConnectionResponse, TypeConfigurationModel> {
+class Resource extends AbstractBigIdResource<ResourceModel, Connection, Connection, Connection, TypeConfigurationModel> {
 
-    async create(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<ConnectionResponse> {
-        const sessionToken = await this.getSessionToken(typeConfiguration);
+    async get(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<Connection> {
+        const response = await new BigIdClient(typeConfiguration.bigIdAccess.domain, typeConfiguration.bigIdAccess.username, typeConfiguration.bigIdAccess.password).doRequest<ConnectionResponse>(
+            'get',
+            `/api/v1/ds_connections/${model.name}`
+        );
 
-        const {data} = await axios.post<ConnectionResponse>(`https://${typeConfiguration.bigIdAccess.domain}/api/v1/ds_connections`,
-            {'ds_connection': {
-                    "name": model.name,
-                    "type": "s3",
-                    // "owners": model.owners,
-                    // "differential": model.differential,
-                    // "numberofparsingthreads": model.numberOfParsingThreads,
-                    // "rdb_is_sample_data": model.rdbIsSampleData,
-                    // "include_file_types": model.includeFileTypes,
-                    // "iscredentialsauth": model.isCredentialsAuth,
-                    // "password": model.password,
-                    // "documentordossiername": null,
-                    // "attributename": null,
-                    // "promptname": null,
-                    // "metricname": null,
-                    // "stylename": null,
-                    // "consolidationname": null,
-                    // "metadataaclscanenabled": model.metadataAclScanEnabled,
-                    // "dsaclscanenabled": model.dsAclScanEnabled,
-                    // "enable_acl": null,
-                    // "scanwindowname": "none",
-                    // "isstsauth": model.isStsAuth,
-                    // "iscrossaccountauth": model.isCrossaccountAuth,
-                    // "isiamroleauth": model.isIamroleAuth,
-                    // "isanonymousauth": model.isAnonymousAuth,
-                    // "updatedpasswords": [],
-                    // "security_tier": model.securityTier,
-                    // "ocr_languages": model.ocrLanguages,
-                    // "scanner_strategy": model.scannerStrategy,
-                    // "enabled": model.enabled,
-                    // "keydeserializer": "string",
-                    // "valuedeserializer": "string",
-                    // "credential_id": model.credentialId,
-                    // "certificate_id": model.certificateId,
-                    // "custom_fields": model.customFields
-                }},
-            {headers: {
-                    'Authorization': sessionToken,
-                    'Accept': 'application/json'
-                }});
-
-        return data;
-    }
-
-    async delete(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<void> {
-        const sessionToken = await this.getSessionToken(typeConfiguration);
-
-        await axios.delete<void>(`https://${typeConfiguration.bigIdAccess.domain}/api/v1/ds_connections/${model.name}`,
-            {headers: {
-                    'Authorization': sessionToken,
-                    'Accept': 'application/json'
-                }});
-    }
-
-    async get(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<ConnectionResponse> {
-        const sessionToken = await this.getSessionToken(typeConfiguration);
-
-        const {data} = await axios.get<ConnectionResponse>(`https://${typeConfiguration.bigIdAccess.domain}/api/v1/ds_connections/${model.name}`,
-            {headers: {
-                    'Authorization': sessionToken,
-                    'Accept': 'application/json'
-                }});
-
-
-        return data;
+        return this.payloadToConnection(response.data);
     }
 
     async list(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<ResourceModel[]> {
-        const sessionToken = await this.getSessionToken(typeConfiguration);
+        const response = await new BigIdClient(typeConfiguration.bigIdAccess.domain, typeConfiguration.bigIdAccess.username, typeConfiguration.bigIdAccess.password).doRequest<ConnectionResponses>(
+            'get',
+            '/api/v1/ds_connections'
+        );
 
-        const {data} = await axios.get<ConnectionResponses>(`https://${typeConfiguration.bigIdAccess.domain}/api/v1/ds_connections/`,
-            {headers: {
-                    'Authorization': sessionToken,
-                    'Accept': 'application/json'
-                }});
+        return response.data.ds_connections.map(connectionPayload => new ResourceModel({
+            id: connectionPayload.id_,
+            connection: this.payloadToConnection(connectionPayload)
+        }));
+    }
 
-        return data.ds_connections.map(connection => this.setModelFrom(new ResourceModel(), new ConnectionResponse(connection)));
+    async create(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<Connection> {
+        const response = await new BigIdClient(typeConfiguration.bigIdAccess.domain, typeConfiguration.bigIdAccess.username, typeConfiguration.bigIdAccess.password).doRequest<ConnectionResponse>(
+            'post',
+            `/api/v1/ds_connections`,
+            undefined,
+            JSON.stringify(this.modelToPayload(model))
+        );
+
+        return this.payloadToConnection(response.data.ds_connection);
+    }
+
+    async update(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<Connection> {
+        const response = await new BigIdClient(typeConfiguration.bigIdAccess.domain, typeConfiguration.bigIdAccess.username, typeConfiguration.bigIdAccess.password).doRequest<ConnectionResponse>(
+            'put',
+            `/api/v1/ds_connections/${model.name}`,
+            undefined,
+            JSON.stringify(this.modelToPayload(model))
+        );
+
+        return this.payloadToConnection(response.data.ds_connection)
+    }
+
+    async delete(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<void> {
+        await new BigIdClient(typeConfiguration.bigIdAccess.domain, typeConfiguration.bigIdAccess.username, typeConfiguration.bigIdAccess.password).doRequest(
+            'delete',
+            `/api/v1/ds_connections/${model.name}`
+        );
     }
 
     newModel(partial: any): ResourceModel {
         return new ResourceModel(partial);
     }
 
-    setModelFrom(model: ResourceModel, from: ConnectionResponse | undefined): ResourceModel {
+    setModelFrom(model: ResourceModel, from?: Connection): ResourceModel {
         if (!from) {
             return model;
         }
-        if (from.id) {
-            model.id = from.id;
-        }
-        if (from.certificateId) {
-            model.certificateId = from.certificateId;
-        }
-        if (from.isIamroleAuth) {
-            model.isIamroleAuth = from.isIamroleAuth;
-        }
-
-        if (from.name) {
-            model.name = from.name;
-        }
-
-        if (from.owners) {
-            model.owners = from.owners;
-        }
-
-        if (from.differential) {
-            model.differential = from.differential;
-        }
-
-        if (from.numberOfParsingThreads) {
-            model.numberOfParsingThreads = from.numberOfParsingThreads;
-        }
-
-        if (from.rdbIsSampleData) {
-            model.rdbIsSampleData = from.rdbIsSampleData;
-        }
-
-        if (from.includeFileTypes) {
-            model.includeFileTypes = from.includeFileTypes;
-        }
-
-        if (from.isCredentialsAuth) {
-            model.isCredentialsAuth = from.isCredentialsAuth;
-        }
-
-        if (from.password) {
-            model.password = from.password;
-        }
-
-        if (from.metadataAclScanEnabled) {
-            model.metadataAclScanEnabled = from.metadataAclScanEnabled;
-        }
-
-        if (from.dsAclScanEnabled) {
-            model.dsAclScanEnabled = from.dsAclScanEnabled;
-        }
-
-        if (from.isStsAuth) {
-            model.isStsAuth = from.isStsAuth;
-        }
-        if (from.isCrossaccountAuth) {
-            model.isCrossaccountAuth = from.isCrossaccountAuth;
-        }
-
-        if (from.isAnonymousAuth) {
-            model.isAnonymousAuth = from.isAnonymousAuth;
-        }
-
-        if (from.securityTier) {
-            model.securityTier = from.securityTier;
-        }
-
-        if (from.ocrLanguages) {
-            model.ocrLanguages = from.ocrLanguages;
-        }
-        if (from.scannerStrategy) {
-            model.scannerStrategy = from.scannerStrategy;
-        }
-
-        if (from.enabled) {
-            model.enabled = from.enabled;
-        }
-        if (from.credentialId) {
-            model.credentialId = from.credentialId;
-        }
-
-        if (from.customFields) {
-            model.customFields = from.customFields;
-        }
+        model.connection = from;
         return model;
     }
 
-    update(model: ResourceModel, typeConfiguration: TypeConfigurationModel): Promise<ConnectionResponse> {
-        // I'm not totally sure I understand what to do here - example uses a GET but I think that's wrong
-        // see https://api.bigid.com/paths/ds_connections-connection_id/put
+    private modelToPayload(model: ResourceModel) {
+        const ownersV2 = Array.of(...model.businessOwners || []).concat(...model.itOwners || []);
 
-        return Promise.resolve(undefined);
+        return {
+            type: 'S3',
+            parquetFileRegex: model.parquetFileRegex || null,
+            name: model.name || null,
+            enabled: model.enabled === true ? 'yes' : 'no',
+            friendly_name: model.friendlyName || null,
+            description: model.description || null,
+            awsAuthStrategy: model.awsAuthenticationType || null,
+            aws_role_session_name: model.awsRoleSessionName || null,
+            aws_role_arn: model.awsRoleArn || null,
+            aws_access_key: model.awsAccessKey || null,
+            aws_secret_key: model.awsSecretKey || null,
+            aws_session_token: model.awsSessionToken || null,
+            aws_region: model.awsRegion || null,
+            bucket_name: model.bucketName || null,
+            include_file_types: model.includeExcludeFiles || true,
+            fileTypesToExclude: model.fileTypesToExclude || null,
+            folder_to_scan: model.folderToScan || null,
+            scanner_group: model.scannerGroup || null,
+            testConnectionTimeoutInSeconds: model.testConnectionTimeoutInSeconds,
+            custom_fields: model.customFields ? Array.of(...model.customFields).map(customField => ({
+                field_name: customField.name,
+                field_value: customField.value_,
+                field_type: customField.type_
+            })) : [],
+            owners_v2: ownersV2.map(businessOwner => ({
+                id: businessOwner.id,
+                origin: businessOwner.origin,
+                email: businessOwner.email,
+                type: businessOwner.type_
+            })),
+            location: model.location || null,
+            scope: model.scope || null,
+            security_tier: model.securityTier || '1',
+            comment: model.comments || null,
+            numberOfParsingThreads: model.numberOfParsingThreads || 5,
+            metadataAclScanEnabled: model.metadataAclScanEnabled === true ? 'true' : 'false',
+            dsAclScanEnabled: model.dsAclScanEnabled === true ? 'true' : 'false',
+            is_ocr_enabled: model.enabledOcr === true ? 'true' : 'false',
+            ocr_timeout_in_seconds: model.ocrTimeout || 60,
+            ocr_languages: model.ocrLanguages || 'eng',
+            doc2vec_is_enabled: model.enableClustering || false,
+            classification_is_enabled: model.enableClustering || false,
+            ner_classification_is_enabled: model.enableAdvanceClassifiers || false,
+            Is_sample_folders: model.sampleFolders === true ? 'true' : 'false',
+            folder_percent_to_sample: model.samplePercentage || 5,
+            Is_sample_files: model.sampleFileContent === true ? 'true' : 'false',
+            differential: model.differentialScan || false,
+            is_modified_in_x_days: model.isModifiedInXDays || false,
+            x_last_days: model.xLastDays || 7,
+            scanWindowName: model.scanWindowName || null
+        }
     }
 
-    protected async getSessionToken(typeConfiguration: TypeConfigurationModel): Promise<string> {
-        const {data} = await axios.post<SessionToken>(`https://${typeConfiguration.bigIdAccess.domain}/api/v1/sessions`,
-            {username: typeConfiguration.bigIdAccess.username, password: typeConfiguration.bigIdAccess.password},
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-
-        return data.auth_token;
+    private payloadToConnection(payload: any) {
+        return new Connection({});
     }
 }
 
